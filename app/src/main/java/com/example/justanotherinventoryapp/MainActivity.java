63c7f35686ad7f1ca69ebb9ff6c3871e25b5e394
Jeremy <jzlau3@illinois.edu>
2020-04-26 16:35:19 -0500

    4/26/2020 update 1

--- app/src/main/java/com/example/justanotherinventoryapp/MainActivity.java
+++ app/src/main/java/com/example/justanotherinventoryapp/MainActivity.java
@@ -24,17 +24,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
-
         add = findViewById(R.id.fab);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/justanotherinventoryapp/addNewItem.java
+++ app/src/main/java/com/example/justanotherinventoryapp/addNewItem.java
@@ -7,14 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class addNewItem extends AppCompatActivity {
     // instance variables
-    String name;
-    int quantity;
-    EditText nameInput;
-    EditText quantityInput;
-    Button submitButton;
+    private String name;
+    private int quantity;
+    private EditText nameInput;
+    private EditText quantityInput;
+    private Button submitButton;
+    private TextView errorMsg;
 
     // methods
     @Override
@@ -24,13 +26,25 @@ public class addNewItem extends AppCompatActivity {
         nameInput = findViewById(R.id.itemName);
         quantityInput = findViewById(R.id.itemQuantity);
         submitButton = findViewById(R.id.createButton);
+        errorMsg = findViewById(R.id.quantError);
+        errorMsg.setVisibility(View.INVISIBLE);
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 name = nameInput.getText().toString();
-                quantity = Integer.parseInt(quantityInput.getText().toString());
-                startActivity(new Intent(v.getContext(), MainActivity.class));
-                finish();
+                Boolean isItInt = true;
+                try {
+                    quantity = Integer.parseInt(quantityInput.getText().toString());
+                } catch(Exception e) {
+                    isItInt = false;
+                }
+                if (!(isItInt)) {
+                    errorMsg.setVisibility(View.VISIBLE);
+                } else {
+                    quantity = Integer.parseInt(quantityInput.getText().toString());
+                    startActivity(new Intent(v.getContext(), MainActivity.class));
+                    finish();
+                }
             }
         });
     }
