20809b8c5f6c6b875d9fcf14532ae268292aa4f6
Jeremy <jzlau3@illinois.edu>
2020-04-26 17:03:14 -0500

    4/26/2020 update 2

---
+++ app/src/main/java/com/example/justanotherinventoryapp/myAdaptor.java
@@ -0,0 +1,56 @@
+package com.example.justanotherinventoryapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class myAdaptor extends RecyclerView.Adapter<myAdaptor.MyViewHolder> {
+
+    String[] data1;
+    int[] data2;
+    Context context;
+
+    public myAdaptor(Context getContext, String[] s1, int[] s2) {
+        context = getContext;
+        data1 = s1;
+        data2 = s2;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data2.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView myText1, myText2;
+        ConstraintLayout mainLayout;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myText1 = itemView.findViewById(R.id.myText1);
+            myText2 = itemView.findViewById(R.id.myText2);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+}
+
