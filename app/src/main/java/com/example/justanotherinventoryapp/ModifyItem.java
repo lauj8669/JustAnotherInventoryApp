ef7122bb3e6c5fd5a95e789e847e70c6b7f9fc69
Seongyong36 <seongyong2000@gmail.com>
2020-04-26 16:07:50 -0700

    added an activity

---
+++ app/src/main/java/com/example/justanotherinventoryapp/ModifyItem.java
@@ -0,0 +1,14 @@
+package com.example.justanotherinventoryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifyItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_item);
+    }
+}
