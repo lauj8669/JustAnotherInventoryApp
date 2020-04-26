b7d88865665fbb712f7cd0bb496190fc8f579f09
Jeremy <jzlau3@illinois.edu>
2020-04-26 18:10:50 -0500

    4/26/2020 update 4

--- app/src/main/java/com/example/justanotherinventoryapp/ModifyItem.java
+++ app/src/main/java/com/example/justanotherinventoryapp/ModifyItem.java
@@ -10,5 +10,6 @@ public class ModifyItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_item);
+
     }
 }
